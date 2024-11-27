86cfbcdff2d92458e0d7c70ad6ee39e8ee057df6
Zidane Fr <zidane_fr@student.ub.ac.id>
2024-11-27 15:51:34 +0700

    Sesudah Konversi Fragment

--- Loker/app/src/main/java/zi/mobile/loker/MainActivity2.java
+++ Loker/app/src/main/java/zi/mobile/loker/MainActivity2.java
@@ -1,18 +1,10 @@
 package zi.mobile.loker;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -21,11 +13,10 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-        ImageView imageview2 = findViewById(R.id.imageView2);
+        ImageView imageView2 = findViewById(R.id.imageView2);
 
         Uri selectedImage = Uri.parse(getIntent().getExtras().getString("gambar"));
 
-        imageview2.setImageURI(selectedImage);
+        imageView2.setImageURI(selectedImage);
     }
 }
-
